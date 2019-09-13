b95346c2bb8eef05fc287a933abed5a4ece08c73
JazNH <js13@wildcats.unh.edu>
2019-09-12 21:22:04 -0400

    simple counting practicing

---
+++ CountToTen.py
@@ -0,0 +1,7 @@
+max = 10
+start = 1
+
+while start <= max:
+    print(start)
+    start = start + 1
+
---
+++ countUpToNumberFromUser.py
@@ -0,0 +1,11 @@
+numberToGuess = input("Tell me a number to count to: ")
+int(numberToGuess)
+x = 0
+
+while x < int(numberToGuess):
+    print(x)
+    x = x + 1
+
+print(numberToGuess)
+
+
