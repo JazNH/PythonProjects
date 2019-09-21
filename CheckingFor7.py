538be0321e314ee87d5621982b408c0c5e8007ab
JazNH <js13@wildcats.unh.edu>
2019-09-21 10:45:34 -0400

    7 checking fix

--- CheckingFor7.py
+++ CheckingFor7.py
@@ -2,9 +2,7 @@ seven = 7
 counter = 0
 x = input('Give me a whole number')
 copyOfX = [int(i) for i in str(x)]
-
 for i in copyOfX:
     if (seven == i):
         counter = counter + 1
-
-print ('There are ' + str(counter) + ' in ' + str(x))
+print ('There are ' + str(counter) + ' 7\'s in ' + str(x))
