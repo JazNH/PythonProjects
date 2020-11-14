147d91916c2e1d0fb9e07e2194335fffb1b790ca
JazNH <js13@wildcats.unh.edu>
2020-11-14 14:07:51 -0500

    RandomnessChallenge

---
+++ RandomNumber.py
@@ -0,0 +1,8 @@
+# randomly choose a number from 1 to 100
+import random
+
+def random_number():
+    num1 = random.randint(1, 100)
+    return(int(num1)) #can change return to print to show the number being returned
+
+random_number()
